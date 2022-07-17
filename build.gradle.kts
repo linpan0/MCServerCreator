c7999893e44f680d78c2f0b9c851ef3f33918859
Backword <linpan0328@gmail.com>
2022-07-16 20:13:36 -0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+  kotlin("jvm").version("1.7.10")
+  id("com.github.johnrengelman.shadow").version("7.1.2")
+}
+
+group = "me.backword"
+version = "1"
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation(kotlin("stdlib"))
+  implementation("com.fasterxml.jackson.core:jackson-core:2.13.3")
+  implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
+}
+
+tasks {
+  compileKotlin {
+    kotlinOptions.jvmTarget = "17"
+  }
+
+  shadowJar {
+    manifest.attributes["Main-Class"] = "me.backword.MCServerCreatorKt"
+  }
+}
